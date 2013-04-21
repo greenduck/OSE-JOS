47e6aa20ec9712b2af2f07b525825cd8ef6a0e2e
Andrey Gelman <andrey.gelman@gmail.com>
2013-04-21 22:24:49 +0300

    Exersice 1: alloc and map 'envs'

--- kern/pmap.c
+++ kern/pmap.c
@@ -15,9 +15,10 @@ size_t npages;			// Amount of physical memory (in pages)
 static size_t npages_basemem;	// Amount of base memory (in pages)
 
 // These variables are set in mem_init()
-pde_t *kern_pgdir;		// Kernel's initial page directory
-struct PageInfo *pages;		// Physical page state array
+pde_t *kern_pgdir;			// Kernel's initial page directory
+struct PageInfo *pages;			// Physical page state array
 static struct PageInfo *page_free_list;	// Free list of physical pages
+struct Env *envs;
 
 
 // --------------------------------------------------------------
@@ -151,7 +152,7 @@ mem_init(void)
 
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
-	// LAB 3: Your code here.
+	envs = (struct Env *)boot_alloc(NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -182,7 +183,7 @@ mem_init(void)
 	// Permissions:
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
-	// LAB 3: Your code here.
+	boot_map_region(kern_pgdir, UENVS, ROUNDUP((NENV * sizeof(struct Env)), PGSIZE), PADDR(envs), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
