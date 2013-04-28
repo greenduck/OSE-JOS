a2f7f021d6f032ed3b997733efc5c62cbfc20d29
Andrey Gelman <andrey.gelman@gmail.com>
2013-04-28 10:20:51 +0300

    Lab 3: Exercise 4: Question 2
    	No user process is PERMITTED to perform INT 14.
    	Consequently an attempt to execute INT 14 will lead
    	to 'general protection' exception a.k.a. INT 13.

--- user/softint.c
+++ user/softint.c
@@ -1,4 +1,11 @@
 // buggy program - causes an illegal software interrupt
+/*
+ * In practice this program generates 'general protection exception' - INT 13. 
+ * Q: Why not INT 14, as it is what is written in the code ? 
+ * A: Because no user process is PERMITTED to perform INT 14. 
+ *      The lack of permission causes the exception.
+ *      This theory can easily be tested by setting INT 14's DPL to 3.
+ */
 
 #include <inc/lib.h>
 
