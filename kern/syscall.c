a663e4fcc3f9b21765d5e2b9bf61a6dc6ec261cc
Andrey Gelman <andrey.gelman@gmail.com>
2013-05-13 12:26:42 +0300

    Lab 4: Exercise 8: env_set_pgfault_upcall() system call

--- kern/syscall.c
+++ kern/syscall.c
@@ -149,8 +149,19 @@ out_fail:
 static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
-	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+	struct Env *env;
+	int ret;
+
+	ret = envid2env(envid, &env, true);
+	if (ret) {
+		goto out_fail;
+	}
+
+	env->env_pgfault_upcall = func;
+	return 0;
+
+out_fail:
+	return ret;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -421,6 +432,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 		return sys_exofork();
 	case SYS_env_set_status:
 		return sys_env_set_status((envid_t)a1, (int)a2);
+	case SYS_env_set_pgfault_upcall:
+		return sys_env_set_pgfault_upcall((envid_t)a1, (void *)a2);
 	case SYS_yield:
 		sys_yield();
 		return 0;
