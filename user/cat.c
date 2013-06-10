fe6aac316c5dc96335df6d59e0513b3cc496a938
Andrey Gelman <andrey.gelman@gmail.com>
2013-06-10 23:59:23 +0300

    Fix bug in 'cat' implementation  --  not my code

--- user/cat.c
+++ user/cat.c
@@ -8,9 +8,14 @@ cat(int f, char *s)
 	long n;
 	int r;
 
-	while ((n = read(f, buf, (long)sizeof(buf))) > 0)
-		if ((r = write(1, buf, n)) != n)
-			panic("write error copying %s: %e", s, r);
+	while ((n = read(f, buf, (long)sizeof(buf))) > 0) {
+		r = 0;
+		while ((r = write(1, &buf[r], n)) != n) {
+			panic_if((r < 0), "write error copying %s: %e", s, r);
+			panic_if((r > n), "write error copying %s: bytes copied (%d) > bytes requested (%d)", s, r, n);
+			n -= r;
+		}
+	}
 	if (n < 0)
 		panic("error reading %s: %e", s, n);
 }
